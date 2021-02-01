ba68825feed053bdcfe0a580625eaa00086b7230
SNarboni <sebastien.narboni@hotmail.fr>
2021-02-01 14:43:46 +0100

    fini

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Form from "./component/Form";
+import Submitted from "./component/Submitted";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tkt: 0,
+    };
+  }
+
+  register = () => {
+    if (this.state.tkt === 0) {
+      return <Form compte={this.count} />;
+    } else {
+      return <Submitted />;
+    }
+  };
+
+  count = () => {
+    let counter = this.state.tkt;
+    counter++;
+    this.setState({ tkt: counter });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">{this.register()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/Submitted.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Submitted extends React.Component {
+    render() {
+        return (
+            <div className="row my-bg-gray">
+                <h1>Form Submitted</h1>
+            </div>
+        )
+    }
+}
+
+export default Submitted;
